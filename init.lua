8731981eace5db8112f01fbc1ef12c0b97f67252
Sankar <1890648+srgsanky@users.noreply.github.com>
2024-03-25 11:05:19 -0700

    Added treesitter context

--- init.lua
+++ init.lua
@@ -982,6 +982,8 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  -- Treesitter context for showing current method name as you scroll down
+  { 'nvim-treesitter/nvim-treesitter-context', opts = {} },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
