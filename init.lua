8f70f98416593de33ebf5e59306dcfa8bdb8eb65
Sankar <1890648+srgsanky@users.noreply.github.com>
2024-03-26 11:03:02 -0700

    Set wrapscan to false

--- init.lua
+++ init.lua
@@ -161,6 +161,9 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Don't wrap around searches
+vim.opt.wrapscan = false
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
