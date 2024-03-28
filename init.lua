e1e834fce9f623d2c876e2f24f255f8b6a9c835c
Sankar <1890648+srgsanky@users.noreply.github.com>
2024-03-28 00:08:57 -0700

    Avoid noice error on load in md files

--- init.lua
+++ init.lua
@@ -1227,6 +1227,9 @@ require('lazy').setup({
           },
         },
         lsp = {
+          signature = {
+            enabled = false, -- Disabling this to avoid error on load in markdown files
+          },
           -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
           override = {
             ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
