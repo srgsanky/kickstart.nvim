4d5a53594134ed8a6d3c23739c3eda5c23aad607
Sankar <1890648+srgsanky@users.noreply.github.com>
2024-03-27 22:39:48 -0700

    Use Noice to show the command line at the top

--- init.lua
+++ init.lua
@@ -1156,12 +1156,55 @@ require('lazy').setup({
   },
 
   -- Terminal in nvim
+  -- If you accidentally clear the screen using cmd + k, you can redraw neovim
+  -- using :mode or ctrl + L (i.e. ctrl + shift + L).
+  -- https://neovim.io/doc/user/various.html
   {
     'akinsho/toggleterm.nvim',
     opts = {},
     config = function()
       require('toggleterm').setup {
         open_mapping = [[<leader>tt]],
+      }
+    end,
+  },
+
+  -- Show command line at the center along with enhancements to notifications
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('noice').setup {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+        notify = {
+          enabled = true,
+        },
       }
     end,
   },
