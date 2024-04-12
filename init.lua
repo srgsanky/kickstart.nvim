c7574237e5ff6ba5db3da8a0d921bdb45c1ebeff
Sankar <1890648+srgsanky@users.noreply.github.com>
2024-04-11 22:29:48 -0700

    Use rust_analyzer from rustup instead of the mason one

--- init.lua
+++ init.lua
@@ -921,7 +921,7 @@ require('lazy').setup({
       local servers = {
         -- gopls = {},
         -- pyright = {},
-        rust_analyzer = {}, -- For rust
+        -- rust_analyzer = {}, -- NOTE: Don't use the rust analyzer from mason. Use rust-analyzer from your rustup toolchain
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
