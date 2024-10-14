93c47773cca251f295eced4ec789e14adbe930e4
Sankar Shanmugam <1890648+srgsanky@users.noreply.github.com>
2024-10-13 21:11:55 -0700

    Disable auto session save. Outline is causing trouble with session

--- init.lua
+++ init.lua
@@ -2252,7 +2252,7 @@ require('lazy').setup({
   {
     'hedyhli/outline.nvim',
     lazy = true, -- This sets the proper highlight groups that will color the current item on the outline
-    cmd = { 'Outline', 'OutlineOpen' },
+    cmd = { 'Outline', 'OutlineOpen', 'OutlineClose' },
     keys = { -- Example mapping to toggle outline
       -- With !, focus stays in the current buffer. Without it, focus moves to Outline.
       -- If I open outline, I want to focus it, so I am not using !.
@@ -3382,15 +3382,15 @@ require('lazy').setup({
       -- List of file types to bypass auto save when the only buffer open is one of the file types listed
       bypass_save_filetypes = aux_windows,
 
-      auto_save = true, -- Enables/disables auto saving session on exit
-      auto_restore = true, -- Enables/disables auto restoring session on start
-      auto_create = true, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
+      auto_save = false, -- Enables/disables auto saving session on exit
+      auto_restore = false, -- Enables/disables auto restoring session on start
+      auto_create = false, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
 
       close_unsupported_windows = true, -- Close windows that aren't backed by normal file before autosaving a session
 
       pre_save = {
         'tabdo Neotree close', -- Close Neotree before saving session
-        'tabdo OutlineClose', -- Close Outline
+        -- 'tabdo OutlineClose', -- Close Outline
         'cclose', -- Close qf list
         'lclose', -- Close location list
       },
