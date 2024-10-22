1344521cea55ff594d5153077118f13745ae681b
Sankar Shanmugam <1890648+srgsanky@users.noreply.github.com>
2024-10-22 08:41:37 -0700

    Disable auto session save and restore.
    This is causing me more trouble lately. Need to investigate before
    enabling it again.

--- init.lua
+++ init.lua
@@ -3511,6 +3511,7 @@ require('lazy').setup({
   -- :SessionDelete if you get error about restoring a session
   {
     'rmagatti/auto-session',
+    enabled = false,
     lazy = false,
     dependencies = {
       'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
