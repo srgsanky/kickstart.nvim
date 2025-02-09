af56a155fb92e7ad33bb4cef50501c4df512206f
Sankar Shanmugam <1890648+srgsanky@users.noreply.github.com>
2025-02-09 11:48:09 -0800

    Add support to render images directly in neovim

--- init.lua
+++ init.lua
@@ -4795,6 +4795,56 @@ require('lazy').setup({
       vim.g.csv_highlight_column = 'y'
     end,
   },
+  {
+    -- I manually cloned it using
+    -- git clone git@github.com:3rd/image.nvim.git  /Users/sanka/.local/share/nvim/lazy/image
+    '3rd/image.nvim',
+    -- Enable only when using kitty term. Even though wezterm support kitty protocol (https://github.com/wezterm/wezterm/issues/986), the
+    -- render is not perfect and is slow.
+    enabled = vim.env.TERM == 'xterm-kitty',
+    config = function()
+      -- Only setup if we're in Kitty terminal
+      if vim.env.TERM == 'xterm-kitty' then
+        require('image').setup {
+          backend = 'kitty',
+          processor = 'magick_rock', -- or "magick_cli"
+          integrations = {
+            markdown = {
+              enabled = true,
+              clear_in_insert_mode = false,
+              download_remote_images = true,
+              only_render_image_at_cursor = false,
+              floating_windows = false, -- if true, images will be rendered in floating markdown windows
+              filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
+            },
+            neorg = {
+              enabled = true,
+              filetypes = { 'norg' },
+            },
+            typst = {
+              enabled = true,
+              filetypes = { 'typst' },
+            },
+            html = {
+              enabled = false,
+            },
+            css = {
+              enabled = false,
+            },
+          },
+          max_width = nil,
+          max_height = nil,
+          max_width_window_percentage = nil,
+          max_height_window_percentage = 50,
+          window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+          window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', 'snacks_notif', 'scrollview', 'scrollview_sign' },
+          editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+          tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+          hijack_file_patterns = { '*.png', '*.jpg', '*.jpeg', '*.gif', '*.webp', '*.avif' }, -- render image files as images when opened
+        }
+      end
+    end,
+  },
 }, {
   config = function()
     -- Logic to run after lazy is setup
