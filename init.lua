717747acd9d11e7a500bc661737da8276a04bafb
Sankar S <1890648+srgsanky@users.noreply.github.com>
2026-07-12 19:41:22 -0700

    Customize markdown preview width

--- init.lua
+++ init.lua
@@ -3932,6 +3932,7 @@ require('lazy').setup({
     build = 'cd app && npm install',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_markdown_css = vim.fn.stdpath('config') .. '/markdown-preview.css'
     end,
     config = function()
       vim.g.mkdp_auto_start = 0
